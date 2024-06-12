a2b70d78bd21d0a2ad1c4819d33824c069a9e361
Fintan Ryan <fintonius.mcginty@gmail.com>
2024-06-12 21:56:02 +0100

    initial tests

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,18 @@
-var a = "1";
-console.log('aaa', a);
+var hell = 'world';
+// FUNCTIONS:
+var getFullName = function (name, surname) {
+    return name + ' ' + surname;
+};
+//OBJECTS:
+var user = {
+    name: 'Me',
+    age: 99,
+};
+var user2 = {
+    name: "Jerk",
+};
+var user3 = {
+    name: 'Frank',
+    age: 23,
+};
+// FUNCTIONS IN INTERFACES
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,30 @@
-const a = "1";
-console.log('aaa', a);
+let hell: string = 'world';
+
+// FUNCTIONS:
+const getFullName = (name: string, surname: string) => {
+    return name + ' ' + surname
+};
+
+//OBJECTS:
+
+const user: object = {
+    name: 'Me',
+    age: 99,
+};
+
+const user2 = {
+    name: "Jerk",
+}
+
+// INTERFACES:
+
+interface User {
+    name: string;
+    age?: number;
+}
+const user3: User = {
+    name: 'Frank',
+    age: 23,
+}
+
+// FUNCTIONS IN INTERFACES
